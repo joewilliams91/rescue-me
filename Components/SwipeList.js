9bfdcb974142d1b696e152c505d0fd737d095c80
Ian Ranasinghe <ianranasinghe@hotmail.co.uk>
2019-10-02 21:07:58 +0100

    SwipeCard animation added, basic styling added, prepped to send data to liked dogs list

---
+++ Components/SwipeList.js
@@ -0,0 +1,279 @@
+import React from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Platform,
+  Image,
+  Text,
+  View,
+  Animated,
+  Dimensions,
+  PanResponder
+} from "react-native";
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
+import { bindActionCreators } from "redux";
+
+import axios from "axios";
+
+export default class SwipeList extends React.Component {
+  state = {
+    currentUser: null,
+    dogs: [],
+    isLoading: true,
+    currentIndex: 0,
+    dogId: null
+  };
+  dogID = null;
+  position = new Animated.ValueXY();
+  rotate = this.position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: ["-30deg", "0deg", "10deg"],
+    extrapolate: "clamp"
+  });
+  rotateAndTranslate = {
+    transform: [
+      {
+        rotate: this.rotate
+      },
+      ...this.position.getTranslateTransform()
+    ]
+  };
+  likedOpacity = this.position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: [0, 0, 5],
+    extrapolate: "clamp"
+  });
+  nopeOpacity = this.position.x.interpolate({
+    inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+    outputRange: [5, 0, 0],
+    extrapolate: "clamp"
+  });
+  superLikeOpacity = this.position.y.interpolate({
+    inputRange: [-SCREEN_HEIGHT / 1, 0, SCREEN_HEIGHT / 1],
+    outputRange: [1, 0, 0],
+    extrapolate: "clamp"
+  });
+
+  componentDidMount() {
+    return axios
+      .get("https://us-central1-rescuemetest-4a629.cloudfunctions.net/getDogs")
+      .then(({ data }) => this.setState({ dogs: data.dogs, isLoading: false }));
+  }
+
+  componentWillMount() {
+    this.PanResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        if (gestureState.dx > 120) {
+          Animated.spring(this.position, {
+            toValue: { x: SCREEN_WIDTH + 150, y: gestureState.dy }
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 });
+              console.log(this.dogID, "<-- Swiped Right - Dog ID"); // Swipe righty mctighty
+            });
+          });
+        } else if (gestureState.dx < -120) {
+          Animated.spring(this.position, {
+            toValue: { x: -SCREEN_WIDTH - 200, y: gestureState.dy }
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 });
+              console.log(this.dogID, "<-- Swiped Left - Dog ID");
+            });
+          });
+        } else if (gestureState.dy < -300) {
+          Animated.spring(this.position, {
+            toValue: { y: -SCREEN_HEIGHT - 200, x: gestureState.dx }
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 });
+              console.log(this.dogID, "<-- Superlike - Dog ID");
+            });
+          });
+        } else {
+          Animated.spring(this.position, {
+            toValue: { x: 0, y: 0 },
+            friction: 6
+          }).start();
+        }
+      }
+    });
+  }
+
+  render() {
+    const { currentUser, dogs, isLoading, currentIndex } = this.state;
+
+    if (isLoading) {
+      return (
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="#e64664" />
+        </View>
+      );
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 60 }}></View>
+          <View>
+            <Text>Header must go here</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            {dogs
+              .map((dog, i) => {
+                if (i < currentIndex) {
+                  return null;
+                } else if (i == currentIndex) {
+                  this.dogID = dog.id;
+
+                  return (
+                    <Animated.View
+                      {...this.PanResponder.panHandlers}
+                      key={i}
+                      style={[
+                        this.rotateAndTranslate,
+
+                        {
+                          height: SCREEN_HEIGHT - 210,
+                          width: SCREEN_WIDTH,
+                          padding: 20,
+                          paddingTop: 50,
+                          position: "absolute"
+                        }
+                      ]}
+                    >
+                      <Animated.View
+                        style={{
+                          opacity: this.nopeOpacity,
+                          transform: [{ rotate: "30deg" }],
+                          position: "absolute",
+                          top: 90,
+                          right: 50,
+                          zIndex: 1000
+                        }}
+                      >
+                        <Text
+                          style={{
+                            borderWidth: 2,
+                            borderColor: "white",
+                            color: "white",
+                            fontSize: 32,
+                            fontWeight: "800",
+                            padding: 10
+                          }}
+                        >
+                          NOPE
+                        </Text>
+                      </Animated.View>
+                      <Animated.View
+                        style={{
+                          opacity: this.likedOpacity,
+                          transform: [{ rotate: "-30deg" }],
+                          position: "absolute",
+                          top: 90,
+                          left: 50,
+                          zIndex: 1000
+                        }}
+                      >
+                        <Text
+                          style={{
+                            borderWidth: 2,
+                            borderColor: "white",
+                            color: "white",
+                            fontSize: 32,
+                            fontWeight: "800",
+                            padding: 10
+                          }}
+                        >
+                          LIKED
+                        </Text>
+                      </Animated.View>
+                      <Animated.View
+                        style={{
+                          opacity: this.superLikeOpacity,
+                          position: "absolute",
+                          bottom: 50,
+                          left: 120,
+                          zIndex: 1000
+                        }}
+                      >
+                        <Text
+                          style={{
+                            borderWidth: 2,
+                            borderColor: "white",
+                            color: "white",
+                            fontSize: 32,
+                            fontWeight: "800",
+                            padding: 10
+                          }}
+                        >
+                          SuperLike
+                        </Text>
+                      </Animated.View>
+
+                      <Image
+                        style={{
+                          flex: 1,
+                          height: null,
+                          width: null,
+                          resizeMode: "cover",
+                          borderRadius: 20
+                        }}
+                        source={{ uri: dog.photos[0] }}
+                      />
+                    </Animated.View>
+                  );
+                } else {
+                  return (
+                    <Animated.View
+                      key={i}
+                      style={[
+                        {
+                          height: SCREEN_HEIGHT - 210,
+                          width: SCREEN_WIDTH,
+                          padding: 20,
+                          paddingTop: 50,
+                          position: "absolute"
+                        }
+                      ]}
+                    >
+                      <Image
+                        style={{
+                          flex: 1,
+                          height: null,
+                          width: null,
+                          resizeMode: "cover",
+                          borderRadius: 20
+                        }}
+                        source={{ uri: dog.photos[0] }}
+                      />
+                    </Animated.View>
+                  );
+                }
+              })
+              .reverse()}
+          </View>
+          <View style={{ height: 60 }}></View>
+          <View>
+            <Text>Footer must go here</Text>
+          </View>
+        </View>
+      );
+    }
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
+});
--- navigation/SwitchNavigator.js
+++ navigation/SwitchNavigator.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import { createSwitchNavigator, createAppContainer } from 'react-navigation'
-import Login from '../Components/Login'
-import SignUp from '../Components/SignUp'
-import Main from '../Components/Main'
-import Register from '../Components/Register'
+import React from "react";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import Login from "../Components/Login";
+import SignUp from "../Components/SignUp";
+import Main from "../Components/Main";
+import Register from "../Components/Register";
+import SwipeList from "../Components/SwipeList";
 
 const SwitchNavigator = createSwitchNavigator(
-    {
-        Login: {
-            screen: Login
-        },
-        SignUp: {
-            screen: SignUp
-        },
-        Main: {
-            screen: Main
-        },
-        Register: {
-            screen: Register
-        }
+  {
+    Login: {
+      screen: Login
     },
-    {
-        initialRouteName: 'Login'
+    SignUp: {
+      screen: SignUp
+    },
+    Main: {
+      screen: Main
+    },
+    Register: {
+      screen: Register
+    },
+    SwipeList: {
+      screen: SwipeList
     }
-)
+  },
+  {
+    initialRouteName: "SwipeList"
+  }
+);
 
-export default createAppContainer(SwitchNavigator)
+export default createAppContainer(SwitchNavigator);
