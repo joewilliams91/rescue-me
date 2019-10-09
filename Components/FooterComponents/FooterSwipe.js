1693d952c7775ca34bfd72fbf87b813c07958e20
Ian Ranasinghe <ianranasinghe@hotmail.co.uk>
2019-10-09 01:42:55 +0100

    Dog card requiring functionality

--- Components/FooterComponents/FooterSwipe.js
+++ Components/FooterComponents/FooterSwipe.js
@@ -1,26 +1,61 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from "react-native";
-import SwipeFooterDislike from "./SwipeFooterDislike";
-import SwipeFooterLike from "./SwipeFooterLike";
-import SwipeFooterGoBack from "./SwipeFooterGoBack";
-import SwipeFooterStar from './SwipeFooterStar';
+import React, { Component } from "react";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
 
 class FooterSwipe extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SwipeFooterGoBack />
-        <SwipeFooterDislike />
-        <SwipeFooterLike />
-        <SwipeFooterStar />
+        <TouchableOpacity onPress={() => console.log("swipeLeft")}>
+          <Image
+            source={require("./swipeLeftButton.png")}
+            style={{
+              width: 50,
+              height: 50,
+              alignSelf: "center",
+              margin: 10
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log("superlike")}>
+          <Image
+            source={require("./superLike.png")}
+            style={{
+              width: 30,
+              height: 30,
+              alignSelf: "center",
+              paddingTop: 4,
+              textAlign: "center",
+              margin: 10
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log("swipeRight")}>
+          <Image
+            source={require("./swipeRightButton.png")}
+            style={{
+              width: 50,
+              height: 50,
+              alignSelf: "center",
+              textAlign: "center",
+              margin: 10
+            }}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    alignSelf: "center",
+    justifyContent: "center",
+    width: wp("80")
   }
 });
 
